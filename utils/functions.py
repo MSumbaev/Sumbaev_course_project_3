b165651cf5b63c287402743d704f9531d396bfa7
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-05-23 21:07:18 +0300

    Changed load_data(), added get_sort_executed_operations

--- utils/functions.py
+++ utils/functions.py
@@ -1,6 +1,5 @@
 import json
 
-
 PATH = "/home/msumbaev/PycharmProjects/Sumbaev_course_project_3/data/operations.json"
 
 
@@ -9,17 +8,26 @@ def load_data(path):
     :param path - путь к файлу.json.
     """
     with open(path) as file:
-        data_json = file.read()
-        data = json.loads(data_json)
+        data = json.load(file)
         return data
 
 
-def get_sort_executed_operations(operations_list, number_of_operations=5):
+def get_sort_executed_operations(operations, number_of_operations=5):
     """Возвращает количество последних выполненных операций
-    :param operations_list - список всех операций.
+    :param operations - список всех операций.
     :param number_of_operations - количество последних операций (по умолчанию 5).
     """
-    pass
+    # operations_executed = [operation for operation in operations if operation["state"] == "EXECUTED"]
+    operations_executed = []
+
+    for op in operations:
+
+        if op.get("state", False) and op["state"] == "EXECUTED":
+            operations_executed.append(op)
+
+    sorted_operations_executed = sorted(operations_executed, reverse=True, key=lambda d: d["date"])
+    last_operations = [sorted_operations_executed[i] for i in range(number_of_operations)]
+    return last_operations
 
 
 def print_operations(operations):
