04380d73513179b35dc177def2eabc0b4dec2950
m.sumbaev <sumbaevmaksim98@gmail.com>
2023-05-24 22:36:13 +0300

    Changed functions.py

--- utils/functions.py
+++ utils/functions.py
@@ -12,7 +12,7 @@ def load_data(path):
         return data
 
 
-def get_sort_executed_operations(operations, number_of_operations=5):
+def get_last_executed_operations(operations, number_of_operations=5):
     """Возвращает количество последних выполненных операций
     :param operations - список всех операций.
     :param number_of_operations - количество последних операций (по умолчанию 5).
@@ -20,22 +20,82 @@ def get_sort_executed_operations(operations, number_of_operations=5):
     # operations_executed = [operation for operation in operations if operation["state"] == "EXECUTED"]
     operations_executed = []
 
-    for op in operations:
+    for operation in operations:
 
-        if op.get("state", False) and op["state"] == "EXECUTED":
-            operations_executed.append(op)
+        if operation.get("state", False) and operation["state"] == "EXECUTED":
+            operations_executed.append(operation)
 
     sorted_operations_executed = sorted(operations_executed, reverse=True, key=lambda d: d["date"])
     last_operations = [sorted_operations_executed[i] for i in range(number_of_operations)]
     return last_operations
 
 
-def print_operations(operations):
-    """Выводит на экран операции в формате:
+def get_format_data(operation):
+    """Возвращает дату операции в виде: 'число.месяц.год'."""
 
-    'Пример вывода для одной операции:
-    14.10.2018 Перевод организации
-    Visa Platinum 7000 79** **** 6361 -> Счет **9638
-    82771.72 руб.'
+    date_list = operation["date"].split("T")[0].split("-")
+    format_date = ".".join(date_list[::-1])
+    return format_date
+
+
+def get_description(operation):
+    """Возвращает описание операции."""
+
+    return operation["description"]
+
+
+def get_formatted_bank_accounts(operation):
+    """Возвращает форматированную строку о счетах банковской операции.
+
+    При выполнении перевода возвращает строку в виде:
+    Счет / Платежная система XXXX XX** **** XXXX -> Счет **XXXX
+
+    При открытии счета возвращает номер счета в виде:
+    Счет **XXXX
     """
-    pass
+    to_bank_account = operation["to"].split()
+    format_to_bank_account = to_bank_account[0] + " **" + to_bank_account[1][-4:]
+
+    if operation.get("from", False):
+
+        from_ = operation["from"].split()
+        payment_system = ' '.join(from_[:-1])
+        card_num_list = list(from_[-1])
+        len_card_num = len(card_num_list)
+        card_num_list[-len_card_num + 6:-4] = ["*"] * (len_card_num - 10)
+
+        for i in range(4, len_card_num, 5):
+            card_num_list.insert(i, " ")
+
+        format_from = payment_system + " " + "".join(card_num_list)
+
+        return f"{format_from} -> {format_to_bank_account}"
+
+    else:
+        return format_to_bank_account
+
+
+def get_operation_amount(operation):
+    """Возвращает строку с суммой перевода и в какой валюте"""
+    return f'{operation["operationAmount"]["amount"]} {operation["operationAmount"]["currency"]["name"]}'
+
+
+def print_formatted_operations(operations):
+    """Выводит на экран операции.
+    Пример вывода для одной операции:
+
+        14.10.2018 Перевод организации
+        Visa Platinum 7000 79** **** 6361 -> Счет **9638
+        82771.72 руб.
+
+    """
+    for operation in operations:
+        format_date = get_format_data(operation)
+        description = get_description(operation)
+        formatted_bank_accounts = get_formatted_bank_accounts(operation)
+        operation_amount = get_operation_amount(operation)
+
+        print(format_date, description)
+        print(formatted_bank_accounts)
+        print(operation_amount)
+        print()
